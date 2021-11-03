9bc44489adc992a45c12bfef87a2489ea356a266
vga91 <giuseppe.villani@larus-ba.it>
2021-11-03 09:59:41 +0100

    various Other note changes

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE myDb;
+CREATE TABLE IF NOT EXISTS event (
+	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	  event_data JSON NOT NULL,
+	  version INT,
+	  UNIQUE(aggregate_id, version)
+	);
